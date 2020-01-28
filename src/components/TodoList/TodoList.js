6eefe04ce8fcf971a0440f94e766cd64d76487a3
林强 <linqiang@ctyle.com.cn>
2020-01-28 19:26:26 +0800

    use guide

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 
 export class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    console.log(this.props)
+  }
+  
   render() {
+    
     return (
-      <div>
-     {this.props.name}
+      <div className="shopping-list">
+        <h1>Shopping List for {this.props.name}</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
       </div>
+
     );
   }
 }
