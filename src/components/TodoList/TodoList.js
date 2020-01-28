7fe72b3cae221371875e792b68b505f29c0b5481
林强 <linqiang@ctyle.com.cn>
2020-01-28 19:44:44 +0800

    use map

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -4,18 +4,27 @@ export class TodoList extends Component {
   constructor(props) {
     super(props);
     console.log(this.props)
+    this.state = {
+      list: [
+        { category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football" },
+        { category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball" },
+        { category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball" },
+        { category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch" },
+        { category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5" },
+        { category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7" }
+      ]
+    }
   }
-  
+
   render() {
-    
+    const showList = this.state.list.map(item => {
+      console.log(item)
+      return <h1 key={item.name}>{item.price}</h1>
+    })
     return (
       <div className="shopping-list">
         <h1>Shopping List for {this.props.name}</h1>
-        <ul>
-          <li>Instagram</li>
-          <li>WhatsApp</li>
-          <li>Oculus</li>
-        </ul>
+        {showList}
       </div>
 
     );
