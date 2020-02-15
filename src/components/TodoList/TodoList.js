0403c7cf0aca1c55d673ce76b03cca481885daa0
林强 <linqiang@ctyle.com.cn>
2020-02-15 19:27:19 +0800

    router

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 export class TodoList extends Component {
   constructor(props) {
     super(props);
-    console.log(this.props)
     this.state = {
       list: [
         { category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football" },
@@ -12,19 +11,20 @@ export class TodoList extends Component {
         { category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch" },
         { category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5" },
         { category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7" }
-      ]
+      ],
+      count: 0,
     }
   }
 
   render() {
-    const showList = this.state.list.map(item => {
-      return <div key={item.name}>{item.price}</div>
-    })
+    const showListWithTitle = this.state.list.map(item => {
+      return <div title={item.name} key={item.name}>{item.price}</div>
+    }) 
     return (
       <div className="shopping-list">
         <h1>Shopping List for {this.props.name}</h1>
         <ul>
-          {showList}
+          {showListWithTitle}
         </ul>
 
       </div>
