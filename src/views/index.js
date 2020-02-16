98af24f1092a0081d1051ae29e605b0090eb66c6
林强 <linqiang@ctyle.com.cn>
2020-02-16 18:28:06 +0800

    use redux

---
+++ src/views/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class View extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default View;
