b5eb2df795daae406b62724e811a9a511f69c323
林强 <linqiang@ctyle.com.cn>
2020-02-23 11:22:28 +0800

    use antd lib

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import { TodoList } from './components';
-
+ import { Button } from 'antd'
 function App() {
   return (
     <>
       <TodoList  />
+		<Button type="primary">primary</Button>
     </>
   );
 }
